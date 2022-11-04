18d2468afc88074194aa950e8892a2df624f57b6
rodolfo0259 <contieri.rodolfo@gmail.com>
2022-11-03 21:08:51 -0300

    produtos mais doados graph

--- pages/urls.py
+++ pages/urls.py
@@ -6,4 +6,6 @@ app_name = 'pages'
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('produtos_grafico/', views.produtos_mais_doados, name='produtos_grafico'),
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 
 from users.models  import User
 
@@ -14,3 +14,37 @@ class HomePageView(TemplateView):
                 return redirect('dashboard/')
 
         return super(HomePageView, self).dispatch(request, *args, **kwargs)
+
+
+
+
+####
+from pages.models import DoacaoRecebida
+from django.db.models.aggregates import Sum
+
+
+def query_doacaorecebida():
+    queryset = (DoacaoRecebida.objects
+    .select_related('produto')
+    .values('produto__descricao')
+    .annotate(total_qtd=Sum('quantidade'))
+    .order_by('-total_qtd')
+    )
+    return queryset
+
+
+def produtos_mais_doados(request):
+    labels = []
+    data = []
+
+    for produtos_recebidos in query_doacaorecebida():
+        labels.append(produtos_recebidos['produto__descricao'])
+        data.append(produtos_recebidos['total_qtd'])
+
+    contexto = {
+        'labels': labels,
+        'data': data,
+        'chart_type': 'bar',
+        'legenda': 'Produtos mais doados'
+    }
+    return render(request, 'produtos_grafico.html', contexto)
